844c8c4c7467e9ae19135add02e41f298b9b2c4e
Goran <goran.zdjelar@convergent-usa.com>
2019-07-18 19:12:15 +0200

    Removed extra comma

--- core.js
+++ core.js
@@ -12,7 +12,7 @@ class SiteMapper {
                     pagesDirectory,
                     showIndexInUrl,
                     sitemapPath,
-                    targetDirectory,
+                    targetDirectory
                 }) {
 
                     
